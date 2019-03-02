1c4428ccbdb9eeee2ddb4e6b0e6158516ab9eb57
farshadsafavi <farshadtom2003@gmail.com>
2019-03-02 11:28:59 -0500

    Neural Network Complete

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,8 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h =  sigmoid(X * theta);
+reg = (lambda/(2*m)) * sum(theta(2:end).^2);
+J = (1/m)*sum(-y .* log(h) - (1 - y) .* log(1 - h)) + reg;
 
-
+B = h - y;
+G = (lambda/m)*(theta);
+G(1) = 0;
+grad = (1/m)* X' * B + G;
 
 
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -29,12 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
+predict = sigmoid(X*all_theta');
+[predict_max, p] = max(predict, [], 2);
 
 % =========================================================================
 
