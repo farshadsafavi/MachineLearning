c925107297061e3f238373f8160d9b3c72984d5f
farshadsafavi <farshadtom2003@gmail.com>
2019-03-28 10:00:40 -0400

    Week 8 completed assignment

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -20,9 +20,16 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
+m = size(X, 1);
+disp(size(X));
+for i = 1:m,
+  T = [];
+  for j = 1:K,
+    T = [T;X(i,:)];
+  endfor
+  %sum of squars among exis 2(col)
+  [Min , idx(i)] = min(sum((T - centroids).^2 , 2));
+endfor
 
 
 
